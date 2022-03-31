1b9f1a83c901f702e4814770595b844088de4087
张毅鹏 <243815521@qq.com>
2022-03-31 11:12:13 +0800

    UI修改

--- MyGovernment/Assets/Scripts/Others/TitlePC.cs
+++ MyGovernment/Assets/Scripts/Others/TitlePC.cs
@@ -33,6 +33,10 @@ public class TitlePC : Define
     {
         _maskLayer.ShowAlertOnly(kAlertTypes.Set);
     }
+    public   void OnClickSuggest()
+    {
+        _maskLayer.ShowAlertOnly(kAlertTypes.MessageBoard);
+    }
     void Update()
     {
         
